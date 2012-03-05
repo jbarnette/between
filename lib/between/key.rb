7ad17bcc455cf33a21dbcf4475e9859d464128b4
John Barnette <john@jbarnette.com>
2012-03-05 15:04:55 -0600

    Calculate more in the initializer.

--- lib/between/key.rb
+++ lib/between/key.rb
@@ -10,15 +10,20 @@ module Between
       @context = context
       @data    = data
       @default = options && options[:default]
+
+      # The actual source key. Cache whether or not the calculated key
+      # actually exists in the data.
+
       @source  = (options && options[:from]) || name
+      @source  = @source.to_s unless @data.include? @source
+
       @target  = name.intern
-      @value   = options && options[:value]
-      @value ||= @data[@source] || @data[@source.to_s] || @default
+      @value   = (options && options[:value]) || @data[@source] || @default
+      @exists  = @value || @data.include?(@source) || @default
     end
 
     def exists?
-      !!(@value || @data.include?(@source) ||
-         @data.include?(@source.to_s) || @default)
+      @exists
     end
 
     def set
